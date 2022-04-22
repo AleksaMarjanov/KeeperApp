a16dda14f1fc1543f0cbf879abca2cbd4650dba5
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-04-22 16:30:29 -0500

    rendering all notes from notes.js through mapping and using props

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import Note from './Note';
 import Footer from './Footer';
 import Header from './Header';
+import notes from '../notes';
+
+function createNote(props) {
+    return ( <Note
+        key = {props.key}
+        title = {props.title}
+        content = {props.content}
+        />
+        )
+};
+
 
 export default function App() {
     return (
     <div>
         <Header />,
-        <Note />,
+        {notes.map(createNote)}
         <Footer />
     </div>
     )
