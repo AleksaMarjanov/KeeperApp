816eb1b00c568289f28f5d5b7db47686fc219a72
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-04-22 16:39:10 -0500

    moved function into App comp

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,19 @@ import Footer from './Footer';
 import Header from './Header';
 import notes from '../notes';
 
-function createNote(props) {
-    return ( <Note
-        key = {props.key}
-        title = {props.title}
-        content = {props.content}
-        />
-        )
-};
 
 
 export default function App() {
     return (
     <div>
         <Header />,
-        {notes.map(createNote)}
+        {notes.map((noteItem) => 
+            <Note
+            key = {noteItem.key}
+            title = {noteItem.title}
+            content = {noteItem.content}
+            />
+        )}
         <Footer />
     </div>
     )
